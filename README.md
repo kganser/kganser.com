a9b481b32694b8e1500649430ab46598cc15c130
Klaus Ganser <kganser@kganser.com>
2012-02-15 00:34:29 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+see <http://kganser.com>
---
+++ js/common.js
@@ -0,0 +1,7 @@
+var _gaq = [['_setAccount', 'UA-27434499-1'], ['_trackPageview']];
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
