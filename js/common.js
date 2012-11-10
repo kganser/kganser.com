c38773a21b8efc7fe521aa24446d30ed8a397e7c
Klaus Ganser <kganser@gmail.com>
2012-11-09 17:25:41 -0800

    Add Disqus

--- js/common.js
+++ js/common.js
@@ -1,7 +1,18 @@
-var _gaq = [['_setAccount', 'UA-27434499-1'], ['_trackPageview']];
-
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  
+  var script = function(url) {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = url;
+    document.getElementsByTagName('head')[0].appendChild(s);
+  };
+  
+  // Google Analytics
+  window._gaq = [['_setAccount', 'UA-27434499-1'], ['_trackPageview']];
+  script(document.location.protocol == 'https:' ? 'https://ssl' : 'http://www')+'.google-analytics.com/ga.js');
+  
+  // Disqus
+  script('http://kganser.disqus.com/embed.js');
+  
 })();
